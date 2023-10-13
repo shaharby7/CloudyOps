07dd6bd2aea7003674a53a456158a341e4e269c0
shahar <shahar@hyperguest.com>
2023-10-13 22:23:08 +0300

    fix(cluster): fixing coreds issue on minikube

--- infrastructure/modules/cluster/provider.tf
+++ infrastructure/modules/cluster/provider.tf
@@ -1,3 +1,10 @@
 provider "minikube" {
   kubernetes_version = var.kubernetes_version
 }
+
+provider "kubernetes" {
+  host                   = module.minikube.0.host
+  client_certificate     = module.minikube.0.client_certificate
+  client_key             = module.minikube.0.client_key
+  cluster_ca_certificate = module.minikube.0.cluster_ca_certificate
+}
